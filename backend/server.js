8c518717f60393bb661cc2d5a0cb23b924cd22e4
luciamsotelo <142464028+luciamsotelo@users.noreply.github.com>
2024-04-24 10:21:46 -0700

    fixed routes

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 //server.js file
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,20 +11,30 @@ const PORT = process.env.PORT || 3001;
 // Middleware
 app.use(bodyParser.json());
 app.use(cors());
-app.use(express.static('frontend/build'));
+app.use(express.static("../frontend/build"));
 
 const db = require("./models");
 
 // Routes
 app.get("/:id", (req, res) => {
-  const id = req.params.id
-  res.send(id)
-})
-const userRoutes = require('./routes/userRoutes');
-app.use('/api', userRoutes);
+  const id = req.params.id;
+  res.send(id);
+});
+const userRoutes = require("./routes/userRoutes");
+app.use("/api", userRoutes);
 
-app.get('/', (req, res) =>
-res.sendFile('../frontend/build/index.html'))
+
+app.get('/', (req, res) => {
+  const options = {
+    root: path.join(__dirname, '../frontend/build'),
+    dotfiles: 'deny',
+    headers: {
+      'x-timestamp': Date.now(),
+      'x-sent': true
+    }
+  }
+  res.sendFile('index.html', options)
+})
 
 // Start the server
 app.listen(PORT, async () => {
@@ -37,4 +48,3 @@ app.listen(PORT, async () => {
   }
   console.log(`Server is running on port ${PORT}`);
 });
-
